ceedf334cdf3b742b5c81ca36655936ff465fd8e
unknown <Robin@DESKTOP-T7ER9P6.ucl.ac.uk>
2019-10-11 12:20:50 +0100

    makefile changed

--- OpenEphysLib.cpp
+++ OpenEphysLib.cpp
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <PluginInfo.h>
-#include "NeuropixThread.h"
+#include "Source/NeuropixThread.h"
 #include <string>
 #ifdef WIN32
 #include <Windows.h>
